3dc9130f109f6619e5e5a539dc534c0efa77641e
Alanna <alannapaiva6@gmail.com>
2024-04-05 10:49:18 -0300

    Aula 02

--- src/app/register.tsx
+++ src/app/register.tsx
@@ -1,34 +1,54 @@
-import { View, Image, StatusBar } from "react-native"
-import { FontAwesome, MaterialIcons} from "@expo/vector-icons";
+import { View, Image, StatusBar, Alert } from "react-native";
+import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
 
 import { colors } from "@/styles/colors";
 
 import { Input } from "@/components/input";
 import { Button } from "@/components/button";
-import { Link } from "expo-router";
-import React from "react";
+import { Link, router } from "expo-router";
+import React, { useState } from "react";
 
-export default function Register(){
-    return(
-        <View className="flex-1 bg-green-500 items-center justify-center p-8">
-            <StatusBar barStyle="light-content" />
-            <Image source={require("@/assets/logo.png")} className="h-16" resizeMode="contain"/>
-            <View className="w-full mt-12 gap-3">
-                <Input>
-                    <FontAwesome name="user-circle" size={20} color={colors.green[200]} />
-                    <Input.Field placeholder="Nome completo" />
-                </Input>
-                <Input>
-                    <MaterialIcons name="alternate-email" size={20} color={colors.green[200]} />
-                    <Input.Field placeholder="Email" />
-                </Input>
-                <Button title="Cadastrar"
-                onPress={() => console.warn("clicou")}
-                ></Button>
-                <Link href="/register" className="text-gray-100 text-base font-bold text-center mt-8">
-                    Ainda nao possui ingresso?
-                </Link>
-            </View>
-        </View>
-    )
+export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  function handleRegister() {
+    if (!name.trim() || !email.trim()) {
+      return Alert.alert("Inscrição", "Preencha todos os campos!");
+    }
+
+    router.push("/ticket");
+  }
+
+  return (
+    <View className="flex-1 bg-green-500 items-center justify-center p-8">
+      <StatusBar barStyle="light-content" />
+      <Image
+        source={require("@/assets/logo.png")}
+        className="h-16"
+        resizeMode="contain"
+      />
+      <View className="w-full mt-12 gap-3">
+        <Input>
+          <FontAwesome name="user-circle" size={20} color={colors.green[200]} />
+          <Input.Field placeholder="Nome completo" onChangeText={setName} />
+        </Input>
+        <Input>
+          <MaterialIcons
+            name="alternate-email"
+            size={20}
+            color={colors.green[200]}
+          />
+          <Input.Field placeholder="Email" onChangeText={setEmail} />
+        </Input>
+        <Button title="Realizar inscrição" onPress={handleRegister}></Button>
+        <Link
+          href="/"
+          className="text-gray-100 text-base font-bold text-center mt-8"
+        >
+          Já possui ingresso?
+        </Link>
+      </View>
+    </View>
+  );
 }
